3947e5dc53291d0e6ad1c15ebacae9193415de64
adainese <alberto.dainese@gmail.com>
2022-04-01 14:36:37 +0200

    remove wrong implementation

--- src/main/java/shop/flower/domain/FewerBundlesArrangementPolicy.java
+++ src/main/java/shop/flower/domain/FewerBundlesArrangementPolicy.java
@@ -3,7 +3,6 @@ package shop.flower.domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparingInt;
 import static java.util.Objects.requireNonNull;
@@ -15,36 +14,56 @@ public class FewerBundlesArrangementPolicy implements ArrangementPolicy {
     requireNonNull(availableBundles, "availableBundles can't be null");
 
     var orderedBundles =
-        availableBundles.stream()
-            .sorted(comparingInt(Bundle::size).reversed())
-            .collect(Collectors.toCollection(ArrayList::new));
+        availableBundles.stream().sorted(comparingInt(Bundle::size).reversed()).toList();
 
-    List<Bundle> result;
-
-    do {
-      result = inner(total, orderedBundles);
-      if (result != null) {
-        return result;
-      } else {
-        orderedBundles.remove(0);
-      }
-    } while (!orderedBundles.isEmpty());
-
-    throw new RuntimeException("Cannot fit order");
+    return new DFSListWalker(orderedBundles).arrange(total);
   }
 
-  private List<Bundle> inner(int total, List<Bundle> orderedBundles) {
+  /**
+   * This class is used to allow its parent class to be thread-safe and
+   * to make it easier to swap other algorithms if needed
+   */
+  private record DFSListWalker(List<Bundle> availableBundles) {
 
-    List<Bundle> result = new ArrayList<>();
-    var remain = total;
-
-    for (Bundle bundle : orderedBundles) {
-      while (bundle.size() <= remain) {
-        remain -= bundle.size();
-        result.add(bundle);
-      }
+    sealed interface Result permits Result.Success, Result.Failure {
+      boolean isSuccess();
+      record Success(List<Bundle> bundles) implements Result {
+        @Override public boolean isSuccess() {
+          return true;
+        }
+      };
+      record Failure() implements Result {
+        @Override public boolean isSuccess() {
+          return false;
+        }
+      };
     }
 
-    return remain == 0 ? result : null;
+    public List<Bundle> arrange(int total) {
+      return availableBundles.stream()
+          .map(bundle -> inner(bundle, total))
+          .filter(Result::isSuccess)
+          .map(result -> ((Result.Success)result).bundles)
+          .findFirst()
+          .orElseThrow(() -> new RuntimeException("Cannot fit order"));
+    }
+
+    private Result inner(Bundle bundle, int remain) {
+      if (remain == 0) {
+        return new Result.Success(new ArrayList<>());
+      } else if (remain >= bundle.size()) {
+        for (Bundle children : availableBundles) {
+          if (bundle.size() >= children.size()) {
+            var result = inner(children, remain - bundle.size());
+            if (result instanceof Result.Success success) {
+              success.bundles.add(0, bundle);
+              return success;
+            }
+          }
+        }
+      }
+
+      return new Result.Failure();
+    }
   }
 }
